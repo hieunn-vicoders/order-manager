6183e9ff57905d7f515c765256a276b02d8225ec
taeung123 <youtanlai@gmail.com>
2020-11-19 16:05:49 +0700

    custom email template

--- src/app/Mail/MailNotify.php
+++ src/app/Mail/MailNotify.php
@@ -29,6 +29,9 @@ class MailNotify extends Mailable
      */
     public function build()
     {
-        return $this->subject('Webpress có đơn hàng mới!')->view('order::mail.mail');
+        return $this->get_template('Đơn hàng mới từ webpress.vn !','order::mail.mail',['order',$this->order]);
+    }
+    public function get_template($subject, $view, $with) {
+        return $this->subject($subject)->view($view)->with($with);
     }
 }
