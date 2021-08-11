c8583f012b92437823194e2cc9f409a380813a36
taeung123 <youtanlai@gmail.com>
2021-08-11 14:55:48 +0700

    fix orderItemSeeder

--- src/database/seeds/OrderItemsSeeder.php
+++ src/database/seeds/OrderItemsSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
 use VCComponent\Laravel\Order\Entities\OrderItem;
+use Illuminate\Support\Facades\DB;
 
 class OrderItemsSeeder extends Seeder
 {
@@ -13,18 +14,28 @@ class OrderItemsSeeder extends Seeder
      */
     public function run()
     {
+        $products = DB::table('products')->get();
+        $product_ids = collect($products)->map(function ($item) {
+            return $item->id;
+        });
 
+        $orders = DB::table('orders')->get();
+        $order_items = collect($orders)->map(function ($item) {
+            return $item->id;
+        });
         for ($item = 0; $item < 1000; $item++) {
-            $date = Carbon::create(2021, 8, 5, 0);
-            $order_date = $date->subDay(rand(1, 365));
+            $product_id = $product_ids[array_rand($product_ids->toArray())];
+            $order_id = $order_items[array_rand($order_items->toArray())];
+            $query_product = DB::table('products')->where('id', $product_id)->first();
+            $query_order = DB::table('orders')->where('id', $order_id)->first();
             OrderItem::insert([
                 [
-                    "product_id" => rand(1, 10),
-                    "quantity" =>rand(1, 5),
-                    "price"=>rand(1, 10) * 100000,
-                    "order_id" => rand(1, 1000),
-                    "created_at" => $order_date,
-                    "updated_at" => $order_date
+                    "product_id" => $product_id,
+                    "quantity" => rand(1, 5),
+                    "price" => $query_product->price,
+                    "order_id" => $order_id,
+                    "created_at" => $query_order->created_at,
+                    "updated_at" => $query_order->created_at
                 ]
             ]);
         }
